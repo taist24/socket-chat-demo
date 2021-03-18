22ada37d5a6a046c5d57f239da92ab97bcd90eb0
istiyak <tailoristiyak303@gmail.com>
2021-03-19 02:38:56 +0530

    feat(): initial demo

---
+++ server.js
@@ -0,0 +1,38 @@
+const app = require("express")();
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const httpServer = require("http").Server(app);
+const io = require("socket.io")(httpServer);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+io.engine.generateId = function (req) {
+  return uuidv4();
+};
+
+// For authenticating socket
+// io.use((socket, next) => {
+//   // Example: Fetching token from socket
+//   console.log(socket.handshake.query.token || socket.handshake.auth.token);
+
+//   // Example: Sending error
+//   const err = new Error("Unauthorized");
+//   err.data = { message: "Token is invalid or revoked" };
+//   next(err);
+// });
+
+io.on("connection", async function (socket) {
+  console.log(`User: ${socket.id} = ${socket.client.id} connected`);
+
+  socket.broadcast.emit("client-ids", [...(await io.allSockets())]);
+
+  socket.on("send-message", function (data) {
+    console.log(data);
+  });
+});
+
+const port = 3000;
+
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
